37ee9848eb1f04fd4a78dd2b58eca9627126a038
Carine Batista <carinedcb@hotmail.com>
2022-03-23 17:12:14 -0300

    Adding Grid component

--- Grid.js
+++ Grid.js
@@ -1,5 +1,35 @@
+const GRID_SIZE= 4
+const CELL_SIZE = 20
+const CELL_GAP = 2
+
 export default class Grid{
     constructor(gridElement){
-        
+        gridElement.style.setProperty("--grid-size", GRID_SIZE)
+        gridElement.style.setProperty('--cell-size', `${CELL_SIZE}vmin`)
+        gridElement.style.setProperty('--cell-gap', `${CELL_GAP}vmin`)
+        this.cells = createCellElements(gridElement).map((cellElement, index) => {
+            return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
+        })
     }
+}
+
+class Cell {
+    constructor(cellElement, x, y){
+        this.cellElement = this.cellElement
+        this.x = x
+        this.y = y 
+
+    }
+}
+
+
+function createCellElements(gridElement){
+    const cells =[]
+    for(let i = 0; i < GRID_SIZE * GRID_SIZE; i++){
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+        cells.push(cell)
+        gridElement.append(cell)
+    }
+    return cells
 }
