fa5e22004ca56917e61318e781fc7051f09db142
Carine <carinedcb@hotmail.com>
2022-03-31 09:47:51 -0300

    Adding tiles and numbers

--- Grid.js
+++ Grid.js
@@ -13,18 +13,39 @@ export default class Grid{
             return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
         })
     }
+
+    get #emptyCells(){
+        return this.#cells.filter(cell => cell.tile == null)
+    }
+
+    randomEmptyCell(){
+        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+        return this.#emptyCells[randomIndex]
+    }
 }
 
 class Cell {
     #cellElement
     #x
     #y
+    #tile
 
     constructor(cellElement, x, y){
         this.#cellElement = this.cellElement
         this.#x = x
         this.#y = y 
 
+    }
+
+    get tile(){
+        return this.#tile
+    }
+
+    set tile(value){
+        this.#tile = value
+        if(value == null) return
+        this.#tile.x = this.#x
+        this.#tile.y = this.#y
     }
 }
 
