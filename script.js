efe8ecc7c9a0fdbd4141399a8661354907b9168b
Carine <carinedcb@hotmail.com>
2022-04-03 11:26:10 -0300

    Adding canMove Function

--- script.js
+++ script.js
@@ -15,16 +15,32 @@ const grid = new Grid(gameBoard)
  async function handleInput(e){
 	switch(e.key){
 		case "ArrowUp":
-			moveUp()
+			if(!canMoveUp()){
+				setupInput()
+				return
+			}
+			await moveUp()
 			break
 		case "ArrowDown":
+			if(!canMoveDown()){
+				setupInput()
+				return
+			}
 			await moveDown()
 			break
 		case "ArrowLeft":
+			if(!canMoveLeft()){
+				setupInput()
+				return
+			}
 			await moveLeft()
 			break
 		case "ArrowRight":
-			moveRight()
+			if(!canMoveRight()){
+				setupInput()
+				return
+			}
+			await moveRight()
 			break
 		default:
 			setupInput()
@@ -83,3 +99,30 @@ const grid = new Grid(gameBoard)
 		 return promises
 	 }))
  }
+
+ function canMoveUp(){
+	 return canMove(grid.cellsByColumn)
+}
+
+function canMoveDown(){
+	return canMove(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+
+function canMoveRight(){
+	return canMove(grid.cellsByRow.map(row => [...row].reverse()))
+}
+
+function canMoveLeft(){
+	return canMove(grid.cellsByRow)
+}
+
+function canMove(cells){
+	return cells.some(group =>{
+		return group.some((cell, index) => {
+			if(index === 0) return false
+			if(cell.tile === null) return false
+			const moveToCell = group[index - 1]
+			return moveToCell.canAccept(cell.tile)
+		})
+	})
+}
